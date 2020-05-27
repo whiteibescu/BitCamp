bce7a92b0f98abcc5b5bd119fb8dec235b68911c
성수 박 <whiteibescu@gmail.com>
2020-05-27 18:14:20 +0900

    updowngame 오류

---
+++ UpdownCsharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UpdownCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int solution = random.Next() % 1000;
+            while (true)
+            {
+                Console.Write("숫자 입력");
+                int input = int.Parse(Console.ReadLine());
+                if (solution == input)
+                {
+                    Console.WriteLine("정답");
+                    break;
+                }
+                else if (solution == input)
+                {
+                    else if (solution > input)
+                    {
+                        Console.WriteLine(input + "UP");
+                    }
+                    else
+                    {
+                        Console.WriteLine(input + "Down");
+                    }
+                }
+            }
+        }
+    }
+}
