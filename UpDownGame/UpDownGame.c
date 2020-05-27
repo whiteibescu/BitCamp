f8b862784a1408c089ee17018f39b76339be96c5
성수 박 <whiteibescu@gmail.com>
2020-05-27 17:45:17 +0900

    업다운게임 오류

---
+++ UpDownGame/UpDownGame.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(void)
+{
+	int a, b;
+	int play = 1;
+
+	srand(time(NULL));
+	a = rand() % 100 + 1;
+
+	while (play == 1)
+	{
+		printf("1~100사이의 수를 입력해주세요\n");
+		scanf("%d", &b);
+	}
+
+	if (b < 100 && b > 1)
+	{
+		if (b > a)
+		{
+			printf("다운!!\n");
+
+		}
+		else if (b < a)
+		{
+			printf("up!!\n");
+		}
+		else if (a == b)
+		{
+			printf("%정답입니다!!");
+			play = 0;
+		}
+	}
+}
