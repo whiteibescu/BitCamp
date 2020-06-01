2c6f092c5b42c013e34c8020ff77ff4754fe51a9
성수 박 <whiteibescu@gmail.com>
2020-06-01 15:11:30 +0900

    첫 수업 코딩

---
+++ Lecture01/Lecture01.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+#include <string.h> //문자열 처리          // 이 세개는 거의 필수 헤더
+#include <stdlib.h> //기타 등등 유틸리티
+
+#include <math.h> 
+#include <conio.h> //Console Input Output 입출력할때 기계적인 함수 포함
+
+int main(void){ 
+	//첫 실행
+	printf("Hello World!\n");
+	printf("This is not the first program.\n");
+	// 실행항 함수의 헤더 파일을 찾아라...
+
+	return 0;
+}
+
+/* 콘솔 C 언어 프로그램 바느시 main()로 시작*/
+
+/* 소스파일을 생성하고 메인함수를 만든다.
+메인함수는 하나만 있다.
+실행하고자 하는 다 메인함수에 넣어야한다. (밖에는 실행 X) */
+                                            
