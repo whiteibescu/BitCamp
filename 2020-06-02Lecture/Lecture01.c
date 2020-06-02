7db08054a6a24ef8592c694ef2cd229f72cd4656
성수 박 <whiteibescu@gmail.com>
2020-06-02 09:36:36 +0900

    if문과 변수 SWAP

--- 2020-06-02Lecture/Lecture01.c
+++ 2020-06-02Lecture/Lecture01.c
@@ -10,8 +10,23 @@
 int main(void) { 
 	//1 ~ 25 까지 연속하는 정수들을 출력
 
-	int min = 1;	 //입력
+	int min = 100;	 //입력
 	int max = 25;	 //입력
+	// 사용자 순서를 반대로 입력 경우도 생김....
+	// 인지(판단) - 분기문....
+	if (min > max)
+	{
+		// 두변수의 값을 교환한다.
+		
+		int temp = min;
+		min = max;
+		max = temp; 
+		// 스왑하기 위해서는 변수 하나가 더 필요
+		// 비트연산자 XOR 연산 활용법
+
+		ㄹ
+	}
+
 	for (int i = min; i < max+1 ; i++)
 	{
 		printf("%d ", i);
