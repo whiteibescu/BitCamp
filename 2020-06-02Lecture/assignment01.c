d8a795b3aaeda765aaefebfb124b70a6face988c
성수 박 <whiteibescu@gmail.com>
2020-06-02 10:50:41 +0900

    구구단

--- 2020-06-02Lecture/assignment01.c
+++ 2020-06-02Lecture/assignment01.c
@@ -5,16 +5,17 @@
 int main()
 {
     
-    int cnt = 0;
     for (int i = 2; i <= 9; i++)
     {
         
         for (int j = 1; j <= 9; j++)
         {
             printf("%d x %d = %d\n", i, j, (i * j));
+
         }
         printf("\n");
-    }
+        
+    } 
 
     return 0;
 }
