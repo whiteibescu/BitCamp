2515d2d504740011aaf5fa0a876f52a7c26b6267
성수 박 <whiteibescu@gmail.com>
2020-06-03 16:49:54 +0900

    과제

--- 2020-06-03Lecture/Assignment.c
+++ 2020-06-03Lecture/Assignment.c
@@ -67,18 +67,3 @@ int main(void)
 	}
 }
 
-
-/*
-int W = 19;
-int H = 19;
-int omokpan[19 * 19] = { 1,0,1,1,0 };
-
-
-
-// 요소의 값이 0이면 +
-
-for (int r = 0; r < H; ++r) {
-	for (int c = 0; c < W; ++c) {
-
-		int v = omokpan[r * W + c];
-		*/
