a5239ce57e7a75646dd56da0ecd0decd90e83b12
성수 박 <whiteibescu@gmail.com>
2020-06-08 18:00:52 +0900

    과제

---
+++ 2020-06-08Lecture/assignment.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+	char ch;
+	int digit = 0; // 숫자 수
+	int alpha = 0; // 영문자 수
+	int words = 0; // 띄어쓰기 
+	int number = 0; // 정수들의 총합까지
+	while ( (ch = getchar() ) != EOF) {
+
+		
+	}
+	printf("\n");
+
+
+	return 0;
+}
+/*
+갯수 세기
+EOF (Ctrl+Z) 기능 포함...
+
+숫자 수 :	
+
+영문자 수 :
+
+단어수 :
+*/
