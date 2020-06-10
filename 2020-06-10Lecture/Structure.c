daebd192a708cece4381811d0702c21392b6263a
성수 박 <whiteibescu@gmail.com>
2020-06-10 23:51:28 +0900

    최종 수정

--- 2020-06-10Lecture/Structure.c
+++ 2020-06-10Lecture/Structure.c
@@ -4,46 +4,60 @@
 #include <math.h>
 #include <conio.h>
 #include <string.h>
+#include <windows.h>
+
+// #define MAX 5
+#define MAX_LENGTH 20
+
 
 struct person {
-	char name[30];
-	int age;
-	char sex[30];
+    char name[30];
+    int age;
+    char sex[30];
 };
 
 int main(void) {
-	int choose;
-	printf("=========================\n");
-	printf("① 주소록 추가\n\n");
-	printf("② 주소록 보기\n\n");
-	printf("③ 주소록 검색\n");
-	printf("=========================\n");
-	scanf("%d", &choose);
+    int choose;
+    struct person dart[MAX_LENGTH] = { 0, };
+    // int i = 0;
+    int count = 0;
 
+    while (1)
+    {
+        printf("=========================\n");
+        printf("① 주소록 추가\n\n");
+        printf("② 주소록 전체보기\n\n");
+        printf("③ 종료\n");
+        printf("=========================\n\n");
+        scanf("%d", &choose);
+        system("cls");
 
-
-
-
-	struct person dart[2];
-	int i;
-	
-	for (int i = 0; i < 2; ++i) {
-		printf("이름 : ");
-		scanf("%s", &dart[i].name);
-		printf("나이 : ");
-		scanf("%d", &dart[i].age);
-		printf("성별 : ");
-
-		scanf("%s", &dart[i].sex);
-	}
-	
-
-
-	for (int i = 0; i < 2; ++i) {
-		
-		
-		printf("이름 : %s  나이 : %d 성별 : %s\n", dart[i].name, dart[i].age, dart[i].sex);
-	}
-	return 0;
-
+        if (choose == 1) {
+            printf("----주소록 추가중----\n");
+            // count++;
+            if (count <= MAX_LENGTH) {
+                printf("이름 : ");
+                scanf("%s", &dart[count].name);
+                printf("나이 : ");
+                scanf("%d", &dart[count].age);
+                printf("성별 : ");
+                scanf("%s", &dart[count].sex);
+                printf("\n");
+                printf("등록이 완료되었습니다!\n");
+                printf("\n");
+                // i++;
+                count++;
+                choose--;
+            }
+        }
+        if (choose == 2) {
+            for (int i = 0; i < count; ++i) {
+                printf("%d. 이름 : %s  나이 : %d 성별 : %s\n\n\n\n", i + 1, dart[i].name, dart[i].age, dart[i].sex);
+            }
+        }
+        if (choose == 3) {
+            break;
+        }
+    }
+    return 0;
 }
