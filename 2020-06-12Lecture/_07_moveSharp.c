8f47a8094fb7f7dbc7324098bc3bb841190b7f39
성수 박 <whiteibescu@gmail.com>
2020-06-13 19:09:02 +0900

    과제

--- 2020-06-12Lecture/_07_moveSharp.c
+++ 2020-06-12Lecture/_07_moveSharp.c
@@ -1,19 +1,55 @@
-// 로컬 헤더 파이을 아래처럼 포함
 #include "turboc.h"
+
 
 void main()
 {
-	setcursortype(NOCURSOR); // no 커서
+    setcursortype(NOCURSOR); // no 커서
+    int directionX = 0; // 0이면 오른쪽으로 이동
+    int directionY = 0; // 00 이면 남동 01 북동 10 남서 11북서
 
-	for (int x = 0; x <= 80; ++x)
-	{
-		clrscr();				// 화면 청소
-		gotoxy(x, 12);
-		puts("#");
-		delay(100);
-	}
-
-
-
+    int x = 0; // 좌표
+    int y = 0;
+    while (1) {
+        clrscr();
+            if (directionX == 0) {
+                if (x == 80) {
+                    directionX = 1;
+                    continue;
+                }
+                x++;
+            }
+            else {
+                if (x == 0) {
+                    directionX = 0;
+                    continue;
+                }
+                x--;
+            }
+        
+            if (directionY == 0) {
+                if (y == 25) {
+                    directionY = 1;
+                    continue;
+                }
+                y++;
+            }
+            else {
+                if (y == 0) {
+                    directionY = 0;
+                    continue;
+                }
+                y--;
+            }
+        
+        printf("x: %d, y: %d, directionX: %d, directionY: %d", x, y, directionX, directionY);
+        gotoxy(x, y);
+        puts("#");
+        delay(50);
+    }
 
 }
+
+//1. #이 다시 시작위치에서 반복되도록 만드세요.
+//2. #이 왕복하도록 만드세요.
+//3. #이 반사되도록 만드세요. (무한 V자 만들기) 
+//힌트 : x값과 y값을 모두 변화시키면 된다.
--- 2020-06-12Lecture/_08_moveSharp.c
+++ 2020-06-12Lecture/_08_moveSharp.c
@@ -4,19 +4,28 @@
 void main()
 {
 	setcursortype(NOCURSOR); // no 커서
+	while (1) {
 
-	for (int x = 0; x <= 80; ++x)
-	{
-		gotoxy(x, 12);
-		puts("#");
-		delay(100);
-		gotoxy(x, 12);
-		puts(" ");
+		for (int x = 0; x < 80; ++x)
+		{
+			gotoxy(x, 0);
+			puts("#");
+			delay(10);
+			gotoxy(x, 0);
+			puts(" ");
+		}
+
+		for (int x = 80; x >= 0; --x)
+		{
+			gotoxy(x, 0);
+			puts("#");
+			delay(10);
+			gotoxy(x, 0);
+			puts(" ");
+		}
 	}
-
 }
 
 //1. #이 다시 시작위치에서 반복되도록 만드세요.
 //2. #이 왕복하도록 만드세요.
-//3. #이 반사되도록 만드세요. (무한 V자 만들기) 
-//힌트 : x값과 y값을 모두 변화시키면 된다.
+//3. #이 반사되도록 만드세요. (무한 V자 만들기
