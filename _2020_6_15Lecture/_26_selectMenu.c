3363e3b4f394ed151805c729cd569d91ec218e93
성수 박 <whiteibescu@gmail.com>
2020-06-15 15:42:06 +0900

    수업

--- _2020_6_15Lecture/_26_selectMenu.c
+++ _2020_6_15Lecture/_26_selectMenu.c
@@ -2,7 +2,7 @@
 
 void main()
 {
-	int selNum = 0;
+	int selNum = 0;  //4바이트 크기
 	printf("\t<주소록 관리 프로그램>\n");
 	printf("1. 입력 \n");
 	printf("2. 검색 \n");
@@ -10,7 +10,22 @@ void main()
 	printf("4. 삭제 \n");
 	printf("5. 전제출력 \n");
 	printf("6. 종료 \n");
+	
+	printf("\n 번호를 선택하세요 >> ");
+	scanf_s("%d", &selNum); //bufferoverflow 에 취약함..
 
-
-	scanf_s("%d, &selNum");
+	// 3. 처리
+	if (selNum == 1)
+		printf("입력처리하였습니다.");
+	else if (selNum == 2)
+		printf("검색처리하였습니다.");
+	else if (selNum == 3)
+		printf("수정처리하였습니다.");
+	else if (selNum == 4)
+		printf("삭제처리하였습니다.");
+	else if (selNum == 5)
+		printf("출력처리하였습니다.");
+	else if (selNum == 6)
+		printf("종료하였습니다.");
+	
 }
---
+++ _2020_6_15Lecture/_27_selectMenu.c
@@ -0,0 +1,57 @@
+//함수를 만드는 2가지 목적
+//1. 반복되는 코드에 규칙성을 찾아서 
+//변화하는 요소만 매개변수지정해서 묶어준다.
+//(여러 번 사용되니깐 1번만 그룹화시켜서 이름을 부여하면 재사용에 매우 편리하다.
+//2. 업무의 단위를 적절한 단위로 그룹화(함수만듬)하면 전체 흐름을 일목요연하게 정리할 수 있다.
+
+#include <stdio.h>
+
+
+void viewMenu()
+{
+	printf("\t<주소록 관리 프로그램>\n");
+	printf("1. 입력 \n");
+	printf("2. 검색 \n");
+	printf("3. 수정 \n");
+	printf("4. 삭제 \n");
+	printf("5. 전제출력 \n");
+	printf("6. 종료 \n");
+}
+
+int getSelnum()
+{
+	int selNum = 0;
+	printf("\n번호를 선택하세요 >> ");
+	scanf_s("%d", &selNum);
+
+	// 이 함수의 일처리가 끝나면 가지고 돌아갈 값
+	return selNum;
+}
+
+void processWork(int selNum)
+{
+	if (selNum == 1)
+		printf("\n입력처리하였습니다.\n");
+	else if (selNum == 2)
+		printf("\n검색처리하였습니다.\n");
+	else if (selNum == 3)
+		printf("\n수정처리하였습니다.\n");
+	else if (selNum == 4)
+		printf("\n삭제처리하였습니다.\n");
+	else if (selNum == 5)
+		printf("\n출력처리하였습니다.\n");
+	else if (selNum == 6)
+		printf("\n종료하였습니다.\n");
+}
+
+
+void main()
+{
+	int selNum = 0;  
+	
+	viewMenu();				// 1. 메뉴 보여주기
+	selNum = getSelnum();	// 2. 사용자 입력
+	processWork(selNum);	// 3. 처리
+}
+
+
